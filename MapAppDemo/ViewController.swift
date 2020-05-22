6e46e15e1dbe9bbe96f044be221c61d77325f077
SokiKawashima <mokorokokoroko@gmail.com>
2020-05-22 10:02:49 +0900

    お願いします。

--- MapAppDemo/ViewController.swift
+++ MapAppDemo/ViewController.swift
@@ -7,14 +7,52 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    var myLock = NSLock()
+    let goldenRatio = 1.618
+    
+@IBOutlet var mapView: MKMapView!
+    var locationManager: CLLocationManager!
 
+    @IBAction func clickZoomin(_ sender: Any) {
+        myLock.lock()
+        if (0.005 < mapView.region.span.latitudeDelta / goldenRatio) {
+            var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
+            regionSpan.latitudeDelta = mapView.region.span.latitudeDelta / goldenRatio
+                mapView.region.span = regionSpan
+            }
+            myLock.unlock()
+            
+    }
+    @IBAction func clickZoomout(_ sender: Any) {
+        myLock.lock()
+        if (mapView.region.span.latitudeDelta * goldenRatio < 150.0) {
+            var regionSpan:MKCoordinateSpan = MKCoordinateSpan()
+            regionSpan.latitudeDelta = mapView.region.span.latitudeDelta * goldenRatio
+            mapView.region.span = regionSpan
+        }
+        myLock.unlock()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        
+        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+        
+        mapView.showsUserLocation = true
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+    mapView.setCenter((locations.last?.coordinate)!, animated: true)
+}
 
 }
+
 
