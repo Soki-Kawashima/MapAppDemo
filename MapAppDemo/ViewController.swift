9fc59a930b6411471fa70822f82cf8ef07de598c
SokiKawashima <mokorokokoroko@gmail.com>
2020-05-22 09:33:48 +0900

    Initial Commit

---
+++ MapAppDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapAppDemo
+//
+//  Created by 川島壮生 on 2020/05/22.
+//  Copyright © 2020 川島壮生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
